0848cd0c0cd7af82ca3e346e1acab7bf1e20ffba
JuanAlviades <jcamilo.01_@hotmail.com>
2024-02-20 14:22:15 -0500

    Creación dé rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { IphoneComponent } from './iphone/iphone.component';
+import path from 'path';
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent,
+    },
+
+    {
+        path: 'iphone', component: IphoneComponent,
+    }
+    
+];
--- src/app/iphone/iphone.component.ts
+++ src/app/iphone/iphone.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-iphone',
   templateUrl: './iphone.component.html',
-  styleUrl: './iphone.component.scss'
+  styleUrl: './iphone.component.scss',
 })
 export class IphoneComponent {
 
